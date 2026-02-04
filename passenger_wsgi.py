8522dc0c0a2228e3a26fea921bdd23cf11a0ed6c
Keyroamos <amosorupia24@gmail.com>
2026-02-04 10:57:44 +0300

    c

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,13 @@
-import os
 import sys
+import os
+
+# INTERP is the path to the python interpreter in your virtualenv
+# Derived from: /home/bdmiscok/virtualenv/public_html/nedbdmis/3.9/bin/activate
+INTERP = "/home/bdmiscok/virtualenv/public_html/nedbdmis/3.9/bin/python"
+
+# Check if we are running the correct interpreter, if not, restart with the correct one
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
 
 # Add the project directory to the sys.path
 sys.path.insert(0, os.path.dirname(__file__))
